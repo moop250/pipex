a171c5c049da9b5e6fecc64343352d3f31dc22e9
hlibine <hugo.libine@gmail.com>
2024-02-17 01:58:17 +0100

    more heredoc improvements

--- bonus/pipex_bonus.c
+++ bonus/pipex_bonus.c
@@ -6,7 +6,7 @@
 /*   By: hlibine <hlibine@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/14 23:52:06 by hlibine           #+#    #+#             */
-/*   Updated: 2024/02/17 01:44:07 by hlibine          ###   ########.fr       */
+/*   Updated: 2024/02/17 01:48:00 by hlibine          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,24 +17,19 @@ void	heredoc(t_key *key)
 {
 	char	*line;
 	char	*out;
-	int		flag;
 	char	*tmp;
 
 	out = ft_strdup("");
-	flag = 0;
-	while (flag != 1)
+	line = ft_strdup("");
+	while (ft_strncmp(key->av[2], line, ft_strlen(line) - 1) != 0)
 	{
-		line = get_next_line(0);
-		if (ft_strncmp(key->av[2], line, ft_strlen(line) - 1) != 0)
-		{
-			tmp = ft_strjoin(out, line);
-			gfree(out);
-			out = tmp;
-		}
-		else
-			flag = 1;
 		gfree(line);
+		line = get_next_line(0);
+		tmp = ft_strjoin(out, line);
+		gfree(out);
+		out = tmp;
 	}
+	gfree(line);
 	key->in = open(".swap", O_CREAT | O_RDWR | O_TRUNC, 0644);
 	if (key->in < 0)
 		px_error("Error: Failed to create swap file");
